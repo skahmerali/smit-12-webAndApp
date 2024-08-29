b1a06d276c869d81d7ed6e077e21f91948dde998
mohammad saad ali <>
2024-08-29 12:55:29 +0500

    first class of js

---
+++ js/class-1/app.js
@@ -0,0 +1,22 @@
+// String, Number
+
+var paragraph = "this is Sayani First class of js";
+var firstnumber = 124;
+
+var firstName = "ahmer";
+
+//concatinate + (attach , join, combine)
+// alert("this is my First Name : " + firstName);
+
+// console.log("this is my First Name : Ahmer ");
+// console.log(2 + 7 - 6 / 5 * 4);
+console.log(((450 * 5 ) / 100 ) * 90);
+
+
+alert(50 + " 56");
+var firstName = "ahmer ali";
+
+
+var num2 = 23;
+console.log(num2++);
+console.log(++num2)
