e45fcc99d0fb5161b220ba366b2572b9b0a90656
mohammad saad ali <38328235+skahmerali@users.noreply.github.com>
2024-08-31 12:51:58 +0500

    some code

---
+++ js/class-2/app.js
@@ -0,0 +1,26 @@
+// console.log('Hello Saylani');
+
+// data types : Number String Boolean
+var firstName;
+// assigny Opperator =
+//if the value coat inside the '' "" its called string
+
+firstName = "Ahmer ";
+var lastName = "Ali";
+var numb1 = 1;
+var num2 = 2;
+
+alert(numb1 + num2);
+console.log("Sheikh " + firstName + lastName);
+
+console.log(numb1 + num2);
+console.log(numb1 - "ahmer");
+
+console.log(numb1++); //numb1 = 124 but print 123 because of post increament
+console.log(++numb1);
+// numb1 =1
+// numb2 =2
+console.log(((((numb1++ + num2++) - --numb1) + ++num2) - --numb1));
+console.log(num2)
+// 1 + 2 - 1 + 4 - 0 = 6
+//           1+ 2 - 1 +     - 0
