949bb5f97dbdc5cb540d1e4190451fb78181c417
mabdullahjs <38328235+skahmerali@users.noreply.github.com>
2024-09-24 13:03:51 +0500

    Add Assignments

---
+++ js/patterns/app.js
@@ -0,0 +1,42 @@
+// STUDENTS TASKS PACTICE
+
+
+// for(var i=1; i<=10; i++){
+//     for (var j=1; j<=i; j++){
+//         document.write("*")
+//     }
+//     document.write("<br>")
+// }
+
+
+//var stars = ['*'];
+
+//for (var i = 1; i <= 5; i++) {
+
+  //  for (var j = 1; j <= i; j++){
+    //    console.log(stars);
+      //  stars.push('*');
+        //stars.unshift('*');
+
+    //}
+//}
+
+// var stars = ['*']
+// for (var i=1; i<=5;i++) {
+
+//     for (var j=1; j<=i;j++){
+//         console.log('*');
+//         stars.push('*')
+//     }
+// }
+
+// var i;
+// var j;
+// for (i=0; i<5 ; i++){
+//     document.write("<br>"){
+//     for (j=0; j < 6-i; j++)
+//     document.write(&nsd&nsd)
+//     }
+//     for (j=0; j )
+// }
+
