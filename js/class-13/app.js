d8f44a3150abf1356f58cfc398b219511a55e765
skahmerali <38328235+skahmerali@users.noreply.github.com>
2024-10-10 12:58:21 +0500

    function

---
+++ js/class-13/app.js
@@ -0,0 +1,43 @@
+// function inputChecker(e, f) {
+//   if (e > f) {
+//     console.log("hello world", e / f);
+//   }
+//   console.log("hello world", e + f);
+//   console.log("hello world", e - f);
+//   console.log("hello world", e * f);
+// }
+// function inputChecker(e, f) {
+//   var result;
+//   if (e > f) {
+//     result = e - f;
+//     // return result;
+//   } else {
+//     result = "first Value is less than second value";
+//     // return result;
+//   }
+//   return result ;
+// }
+
+// inputChecker('ahmer');
+// inputChecker('ali');
+// inputChecker('sheikh');
+
+// inputChecker(3, 4);
+// inputChecker(2, 5);
+// inputChecker(8, 9);
+// inputChecker();
+// console.log(inputChecker(5, 4));
+// inputChecker(2, 5);
+// inputChecker(8, 9);
+
+// const inputChecker = () => {
+//     console.log("hello world");
+// };
+
+// local vs global
+// function inputChecker() {
+//     let updateCode = "SAYLANI Batch 12";
+//     // console.log(updateCode);
+// }
+// inputChecker();
+// console.log(updateCode);
