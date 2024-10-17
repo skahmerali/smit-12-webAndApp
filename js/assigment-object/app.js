2366e13c76938908217d4993d4aba98fbe18f12f
JaffarAman <38328235+skahmerali@users.noreply.github.com>
2024-10-17 12:57:28 +0500

    quizz prepartion

---
+++ js/assigment-object/app.js
@@ -0,0 +1,52 @@
+// var companies = [
+//   {
+//     name: "Honda",
+//     createdAt: "",
+//     type: "",
+//     products: [
+//       {
+//         name: "civic",
+//         price: "10000000",
+//         unitsNoOfSell: "50",
+//       },
+//       {},
+//     ],
+//   },
+//   {
+//     name: "Suzuki",
+//     createdAt: "",
+//     type: "",
+//     products: [
+//       {
+//         name: "civic",
+//         price: "10000000",
+//         unitsNoOfSell: "50",
+//       },
+//       {},
+//     ],
+//   },
+//   {
+//     name: "Toyota",
+//     createdAt: "",
+//     type: "",
+//     products: [
+//       {
+//         name: "civic",
+//         price: "10000000",
+//         unitsNoOfSell: "50",
+//       },
+//       {},
+//     ],
+//   },
+// ];
+
+// for (var i = 0; i < companies.length; i++) {
+//   console.log(companies[i]);
+//   for (var j = 0; j < companies[i].products.length; j++) {}
+// }
+
+if (3 > 2 > 1 === false) {
+  console.log("this is right");
+}else{
+  console.log("this is wrong")
+}
