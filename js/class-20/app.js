e703ac4e7ba3736b65b9823b6166983dbfdad618
sk ahmer ali <38328235+skahmerali@users.noreply.github.com>
2024-11-14 12:50:49 +0500

    DOM Methods

---
+++ js/class-20/app.js
@@ -0,0 +1,34 @@
+// function saveData (email, firstName, lastName, password){
+
+// }
+
+// var userEmail;
+// var userFName;
+// var userLName;
+// var userPassword;
+// saveData(userEmail, userFName, userLName, userPassword);
+// var Email;
+// var FName;
+// var LName;
+// var Password;
+// saveData(Email, FName, LName, Password);
+
+var parentElement = document.getElementById("parent");
+// var parentElement = document.getElementById("parent").childNodes[0];
+// var parentElement = document.getElementById("parent").childNodes[1].childNodes[0];
+// console.log(parentElement.nodeName);
+// console.log(parentElement.nodeType);
+// console.log(parentElement);
+// console.log(parentElement.nodeValue);
+// console.log(parentElement.value);
+
+// console.log(parentElement.hasAttribute("class"))
+
+// var attributeName = parentElement.getAttribute("id");
+// console.log(attributeName);
+// attributeName = parentElement.getAttribute("id");
+// parentElement.setAttribute("class","mainDiv");
+// attributeName = parentElement.getAttribute("class");
+// console.log(attributeName);
+console.log(parentElement);
+parentElement = parentElement.createElement("span");
