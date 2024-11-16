db0774f87fcd597bea728ecc7416b3abd2429a6a
sk ahmer ali <38328235+skahmerali@users.noreply.github.com>
2024-11-16 13:10:17 +0500

    objects starting

---
+++ js/class-21/app.js
@@ -0,0 +1,46 @@
+// var obj = {
+//     name: "Allama Iqbal",
+//     class: "web and app",
+//     education: [
+//         {
+//             graduation: "history",
+//             place: "Aligarh Uni",
+//         },
+//         {
+//             masters: ""
+//         }
+//     ],
+//     Nationality: ["pakistan", ""],
+//     sibings: {
+        
+//     }
+// }
+
+
+// var std = [
+//     {
+//         name: "",
+//         rollNumber: "",
+//         Nationality: ""
+//     },
+//     {
+//         name: "",
+//         rollNumber: "",
+//         Nationality: ""
+//     },
+//     {
+//         name: "",
+//         rollNumber: "",
+//         Nationality: ""
+//     },
+//     {
+//         name: "",
+//         rollNumber: "",
+//         Nationality: ""
+//     },
+//     {
+//         name: "",
+//         rollNumber: "",
+//         Nationality: ""
+//     }
+// ]
