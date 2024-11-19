d87e23248d0c1c4393f32277df8378e0b12a0f39
sk ahmer ali <38328235+skahmerali@users.noreply.github.com>
2024-11-19 12:55:00 +0500

    objects and array example

---
+++ js/Object-Task/app.js
@@ -0,0 +1,191 @@
+var onlineStore = [
+    {
+        name: "TechWorld Online Store",
+        location: "New York, NY",
+        categories: [
+            "Laptops",
+            "Smartphones",
+            "Accessories"
+        ],
+        products: [
+            {
+                id: 1,
+                name: "MacBook Pro",
+                category: "laptop",
+                price: "2500$",
+                stocks: 200,
+                details: {
+                    brand: "apple",
+                    model: "pro 2024",
+                    colors: ["black", "gray", "white"],
+                    screenSize: "14 inch",
+                },
+
+            }
+        ],
+        customers: [
+            {
+                id: 1,
+                firstName: "Waqas",
+                lastName: "Majid",
+                email: "waqas@gmail.com",
+                phoneNumber: "03xxxxxxxx",
+                Address: {
+                    street: "st 1 phool gali",
+                    city: "karachi",
+                    state: "sindh",
+                    zipCode: "70000",
+
+                },
+
+            }
+        ],
+        orders: [
+            {
+                oId: 1,
+                date: 'createdAt()',
+                status: 0, //0 : SHIPPED, 1: DELIVERD, 2: PROCESSING,
+                totalAmount: '2500$',
+                products: [
+                    {
+                        productID: 1,
+                        qantity: 5,
+                        price: '2500$',
+
+                    }
+                ]
+
+
+            }
+        ]
+    },
+    {
+        name: "TechWorld Online Store",
+        location: "New York, NY",
+        categories: [
+            "Laptops",
+            "Smartphones",
+            "Accessories"
+        ],
+        products: [
+            {
+                id: 1,
+                name: "MacBook Pro",
+                category: "laptop",
+                price: "2500$",
+                stocks: 200,
+                details: {
+                    brand: "apple",
+                    model: "pro 2024",
+                    colors: ["black", "gray", "white"],
+                    screenSize: "14 inch",
+                },
+
+            }
+        ],
+        customers: [
+            {
+                id: 1,
+                firstName: "Waqas",
+                lastName: "Majid",
+                email: "waqas@gmail.com",
+                phoneNumber: "03xxxxxxxx",
+                Address: {
+                    street: "st 1 phool gali",
+                    city: "karachi",
+                    state: "sindh",
+                    zipCode: "70000",
+
+                },
+
+            }
+        ],
+        orders: [
+            {
+                oId: 1,
+                date: 'createdAt()',
+                status: 0, //0 : SHIPPED, 1: DELIVERD, 2: PROCESSING,
+                totalAmount: '2500$',
+                products: [
+                    {
+                        productID: 1,
+                        qantity: 5,
+                        price: '2500$',
+
+                    }
+                ]
+
+
+            }
+        ]
+    },
+    {
+        name: "TechWorld Online Store",
+        location: "New York, NY",
+        categories: [
+            "Laptops",
+            "Smartphones",
+            "Accessories"
+        ],
+        products: [
+            {
+                id: 1,
+                name: "MacBook Pro",
+                category: "laptop",
+                price: "2500$",
+                stocks: 200,
+                details: {
+                    brand: "apple",
+                    model: "pro 2024",
+                    colors: ["black", "gray", "white"],
+                    screenSize: "14 inch",
+                },
+
+            }
+        ],
+        customers: [
+            {
+                id: 1,
+                firstName: "Waqas",
+                lastName: "Majid",
+                email: "waqas@gmail.com",
+                phoneNumber: "03xxxxxxxx",
+                Address: {
+                    street: "st 1 phool gali",
+                    city: "karachi",
+                    state: "sindh",
+                    zipCode: "70000",
+
+                },
+
+            }
+        ],
+        orders: [
+            {
+                oId: 1,
+                date: 'createdAt()',
+                status: 0, //0 : SHIPPED, 1: DELIVERD, 2: PROCESSING,
+                totalAmount: '2500$',
+                products: [
+                    {
+                        productID: 1,
+                        qantity: 5,
+                        price: '2500$',
+
+                    }
+                ]
+
+
+            }
+        ]
+    }
+]
+
+
+for (var i = 0; i < onlineStore.length; i++) {
+    var storeDetails = onlineStore[i];
+    console.log(storeDetails.orders);
+    for (var j = 0; j < storeDetails.length; j++) {
+        // console.log(storeDetails[j].orders)
+    }
+}
