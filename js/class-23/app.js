46e992cfb3fc219e411802b891872dc9c74ae849
Shehzad Iqbal <sheikhahmerali75@gmail.com>
2024-12-03 11:28:54 +0500

    Method

---
+++ js/class-23/app.js
@@ -0,0 +1,21 @@
+// var obj = {
+//     add : function (a, b){
+//         return a + b;
+//     }
+// }
+
+// console.log(obj.add(5, 10));
+// console.log(obj.add(5, 100));
+// const date = new Date();
+// console.log(date);
+// const today = date.getDay();
+// console.log(today);
+// function Calculation(firstValue, secondValue){
+//     this.firstValue = firstValue;
+//     this.secondValue = secondValue;
+//     return firstValue + secondValue 
+//     // this.add = function () {
+//     //    return firstValue + secondValue ;
+//     // }
+// };
+// console.log(new Add(200, 500).add());
