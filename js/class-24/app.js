27cf808d9b459699487ec18fd846eb4ddd17bcdb
Shehzad Iqbal <sheikhahmerali75@gmail.com>
2024-12-03 13:06:15 +0500

    quiz task

---
+++ js/class-24/app.js
@@ -0,0 +1,39 @@
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(() => console.log(i), 1);
+//     console.log('hello world');
+// }
+// console.log('hello world', 5);
+
+// for (let i = 0; i < 3; i++) {
+//       console.log('hello world', 8);
+//       setTimeout(() => console.log(i), 1);
+//       console.log('hello world', 10);
+//   }
+
+// console.log(helloWorld);
+// let helloWorld = 'hello world 14'
+// function abc (){
+//     // console.log(helloWorld);
+//     helloWorld = 'hello world 17'
+
+// }
+// // console.log(helloWorld);
+// console.log('hello world', 21)
+// console.log(abc());
+// console.log('hello world', 23)
+
+
+
+// const shape = {
+//     radius: 10,
+//     diameter() {
+//         return this.radius * 2;
+//     },
+//     // perimeter: () => { return (2 * Math.PI * this.radius)},
+//     // perimeter: () => console.log(this.radius),
+// };
+
+
+// console.log(shape.diameter());
+// console.log(shape.perimeter());
+// shape.perimeter();
