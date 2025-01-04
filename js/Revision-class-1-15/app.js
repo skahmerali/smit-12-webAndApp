a49925c7d739d9fafa4075bae3e3909f364edfc6
mabdullahjs <38328235+skahmerali@users.noreply.github.com>
2025-01-04 13:18:57 +0500

    javascript revision

---
+++ js/Revision-class-1-15/app.js
@@ -0,0 +1,50 @@
+// var let const 
+// var firstName = 'John';
+
+// var firstName = 'ahmer';
+
+// firstName = 'ali';
+
+
+// let firstName = 'Ali';
+
+// {
+//     let firstName = 'John';
+//     console.log(firstName);
+// }
+// console.log(firstName);
+
+
+//  ++ -- == ===
+
+var num = 6;
+
+
+
+// console.log(num++, 'line number 22');
+// 7
+// setInterval(()=>{
+//     console.log(num++, 'line number 25');
+// }, 2000);
+// console.log(++num, 'line number 27');
+
+
+// Hani : 6 , 8 , 7
+// Hamza : 6, 8, 7
+// ramiz : 6, 7, 9
+// Waqas : 6, 8, 8
+// zain : 6, 8, 9
+
+
+// switch (num) {
+//     case 12 :
+//         console.log('num is 12');
+//         break;
+//     case 6:
+//         console.log('num is 6');
+//     case 8:
+//         console.log('num is 8');
+// }
+
+// What is a loop
+
