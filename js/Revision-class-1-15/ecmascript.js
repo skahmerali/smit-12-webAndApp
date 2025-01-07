de9a10a82a1dc41f007471da1af6ff7609337df6
mabdullahjs <38328235+skahmerali@users.noreply.github.com>
2025-01-07 13:12:47 +0500

    Ecma Script revision classes

---
+++ js/Revision-class-1-15/ecmascript.js
@@ -0,0 +1,55 @@
+
+// abc('ahmer', 10);
+// function abc (userName, userAge){
+//     console.log(userName);
+//     console.log(userAge);
+// }
+// abc('ahmer', 10);
+// const abc = (userName, userAge) => {
+//     console.log(userName);
+//     console.log(userAge);
+// }
+// let abc = function (userName, userAge) {
+//     console.log(userName);
+//     console.log(userAge);
+// }
+// console.log(abc);
+// {
+//     let abc = 'ahmer'
+// }
+// console.log(abc);
+
+
+// a();
+// function a() {
+//     var abc = "ahmer";
+//     // {
+//         b(abc)
+//     // }
+//     console.log("hello shariq");
+// }
+
+// function b(f) {
+//     console.log(f)
+// }
+// var abc = "Welcome to the SAYLANI"
+// console.log("hello this is sheikh ahmer" + "      " + abc);
+
+// console.log(`hello this is sheikh ahmer             ${abc}`)
+
+// var arrays = ["umar", "shoail ahmed", "mashood"];
+
+// for(var i = 0; i < arrays.length; i++ ){
+//     console.log(arrays[i]);
+// }
+
+// const result = arrays.map((data) => {
+    // console.log(data);
+    // return data === 'shoail ahmed' ? data : null
+// })
+
+// console.log(result);
+
+var name = "sheikh ahmer ali"
+
+
