96c8693414693e7659699e433b73cfd98259a61d
mabdullahjs <38328235+skahmerali@users.noreply.github.com>
2025-01-18 13:08:22 +0500

    class-2 typescript

---
+++ typescript/class-2/app.js
@@ -0,0 +1,17 @@
+// let userName:any = "John";
+// userName = 25;
+// let userName:unknown = "John";
+// userName = 25;
+// let userName;
+// if(typeof userName === "string") {
+//     console.log("userName is a string");
+// }else {
+//     console.log("userName is a number");
+// }
+var userValue = 'ahmer';
+if (typeof userValue === "string") {
+    console.log("userName is a string");
+}
+else {
+    console.log("userName is a number");
+}
---
+++ typescript/class-2/app.ts
@@ -0,0 +1,18 @@
+// let userName:any = "John";
+// userName = 25;
+// let userName:unknown = "John";
+// userName = 25;
+// let userName;
+
+// if(typeof userName === "string") {
+//     console.log("userName is a string");
+// }else {
+//     console.log("userName is a number");
+// }
+
+let userValue = 'ahmer';
+if (typeof userValue === "string") {
+    console.log("userName is a string");
+} else {
+    console.log("userName is a number");
+}
